a3f6c5ab7e58fc8d79766f97db64b6ecdd3c472e
Dhrumil <dhrumilvaghela22@gmail.com>
2026-03-18 09:12:40 +0530

    fourth commit

--- T1/LEC4/f3.js
+++ T1/LEC4/f3.js
@@ -2,4 +2,9 @@ var ps= require("fs");
 
 // ps.mkdirSync("node")
 
-ps.writeFileSync("node/write.txt","Hello JS!")
+// ps.writeFileSync("node/write.txt","Hello JS!")
+
+// ps.appendFileSync("node/write.txt","Hello JS!")
+
+data = ps.readFileSync("node/write.txt")
+console.log(data)
--- T1/LEC4/readme.md
+++ T1/LEC4/readme.md
@@ -2,9 +2,9 @@
 
 ##  SYNTAX
 
->[!MAKE_DIRECTOPRY]
->var ps= require("fs");
->ps.mkdirSync("node")
+> [!MAKE_DIRECTOPRY]
+> var ps= require("fs");
+> ps.mkdirSync("node")
 
 > [!NOTE]
 > Useful information that users should know, even when skimming content.
