9620f107860d021fab6c2d57cd37431b1ae0a9f7
Dhrumil <dhrumilvaghela22@gmail.com>
2026-03-18 17:27:04 +0530

    que commit

---
+++ T1/LEC4/Q3.js
@@ -0,0 +1,25 @@
+//  write a node.js script to write the text 'you are creating a file.' to 'help.txt' file. 
+// after that append the text 'you are appending the data.'  to the same file.
+// after that read the file and print on console. 
+// at last print 'thanks for using async programming.' 
+// all oprations are async
+
+
+const { error } = require('console');
+var  q3 = require('fs');
+
+q3.writeFile('help.txt','you are creating a file.',(error)=>
+        {
+            if(error) throw error;
+            q3.appendFile('help.txt','you are appending the data.',(error)=>
+            {
+                if(error) throw error;
+                q3.readFile('help.txt','utf-8',(error,data)=>
+                {
+                    if(error) throw error;
+                    console.log(data)
+                    console.log('thanks for using async programming.')
+                });
+            });
+        });
+ 
