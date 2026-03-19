3a8f027f93face26444a7333568fbc3ae654f928
Dhrumil <dhrumilvaghela22@gmail.com>
2026-03-19 11:52:01 +0530

    new day lec 5 q1

---
+++ T1/LEC5/Q1.js
@@ -0,0 +1,10 @@
+// json processing
+// write a node js program using sync. fs module to manage student data. 
+// 1.create a dir. named "StudentData" 
+// 2.insdie the ditr crate the json file "students.json" containg student record with 'name' and 'marks' [array of 3 objects.] 
+// 3. read the json file.
+// 4. dispaly students whose score more than 75 marks.
+// 5. delete file and folder.
+
+
+
