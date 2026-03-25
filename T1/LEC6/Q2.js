08db9eacca93dd144ee09d76022da5584a63c922
Dhrumil <dhrumilvaghela22@gmail.com>
2026-03-25 09:27:32 +0530

    commit of event module

--- T1/LEC6/Q2.js
+++ T1/LEC6/Q2.js
@@ -1,3 +1,4 @@
-//registering multiple eventListerners for diffrent events [myEvent1,myEvent2]
+// Registering multiple eventListerners for diffrent events [myEvent1,myEvent2]
 // 2. removing a specific eventListerner [removeListener] for myevent2. 
-// 3. removing all the listeners
+// 3. removing all the listeners asscocited with myEvent1.[removeAllListener] 
+// 4 triggring eventns and observbing which listeners execute. 
