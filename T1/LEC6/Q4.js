2cedea148b9e32fd58d64d814fc64dd688eb01b8
Dhrumil <dhrumilvaghela22@gmail.com>
2026-03-25 10:25:27 +0530

    this is 2nd time yrr

---
+++ T1/LEC6/Q4.js
@@ -0,0 +1,28 @@
+// write node js scrit to handle a event to wirte data infile append data in  file and then read the file and display data in console 
+var ps = require("fs");
+
+const eventemmiter = require('events');
+const ee = new eventemmiter();
+
+ee.on('write',function(){
+    ps.writeFile('abc.txt','hello',(err)=>{
+        if(err) throw err;
+        console.log("File created");
+        ee.emit("append")
+    });
+});
+ee.on('append',function(){
+    ps.appendFile('abc.txt','Byee',(err)=>{
+        if(err) throw err;
+        console.log("File Append");
+        ee.emit("read")
+    });
+});
+ee.on('read',function(){
+    ps.readFile('abc.txt',"utf-8",(error,data1) =>{ 
+        if (error) throw error;
+            console.log("data from file : ",data1);
+    });
+});
+
+ee.emit('write')
