f4c293f24906d35eabf46d780512178434efd009
Dhrumil <dhrumilvaghela22@gmail.com>
2026-03-28 08:49:29 +0530

    this si lec 8 the last

--- T1/LEC7/f1.js
+++ T1/LEC7/f1.js
@@ -1,4 +1,4 @@
 var u = require("url");
-var adder = 'https://user:/23@video:8080/video?doc=-01&hL=en#Hello';
+var adder = 'https://user:123@video:8080/video?doc=-01&hL=en#Hello';
 var q =u.parse(adder)
 console.log(q);
