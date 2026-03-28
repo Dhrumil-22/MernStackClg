4bb7f9b1c9b3e3c4fe03e488f1717b49908bb3cb
Dhrumil <dhrumilvaghela22@gmail.com>
2026-03-28 09:09:32 +0530

    this si lec 8 the last

--- T1/LEC7/Q1.js
+++ T1/LEC7/Q1.js
@@ -3,9 +3,10 @@ var u = require('url');
 var adder  = "http://localhost:8000/test?m1=50&m2=60&m3=70";
 var ans = u.parse(adder,true);
 console.log(ans)
+console.log(typeof(ans.query.m1));
+
 var m1 = parseInt(ans.query.m1) 
 var m2 = parseInt(ans.query.m2) 
 var m3 = parseInt(ans.query.m3) 
 var avg = (m1+m2+m3)/3
-
 console.log(avg)
