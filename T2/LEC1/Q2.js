560a79d0b0de41b155b33e71ec10dcbcf296cb27
Dhrumil <dhrumilvaghela22@gmail.com>
2026-04-15 08:58:47 +0530

    this is t2 phase lec 2

---
+++ T2/LEC1/Q2.js
@@ -0,0 +1,11 @@
+// write an express.js script to define one json array of 3 objctes havin properties name and age.
+// sort this object according to age.
+//  if user req "sored" in url then all name along with age should be printed according to desendin order to GE.
+//  ALSO DISPLAY THIS SORTED VLUE ON SORT PAGE AND DISPLAY JSON OBJET ON HOME PAGE
+
+var express = require("express");
+var app = express()
+app.get('/',(req,res)=>{});
+
+
+
