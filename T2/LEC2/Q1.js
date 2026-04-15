1aa62eaa9889848a54c6853fc5b1f48394679872
Dhrumil <dhrumilvaghela22@gmail.com>
2026-04-15 10:36:24 +0530

    this is lec @ of T2

--- T2/LEC2/Q1.js
+++ T2/LEC2/Q1.js
@@ -3,3 +3,21 @@
 // 2. sends the form data to the server using a Post req. 
 // 3. the server should read the submited data form the req.body 
 // 4. finally display a confirm msg. 
+
+var express = require("express")
+var app = express()
+app.use(express.urlencoded({extended:true}));
+
+
+app.get("/",(req,res)=>{
+    res.send(`<form action="/login" method="post">
+              <input type="text" name="uname"/>
+              <button type="submit">Click</button>
+              </form>`)
+});
+
+app.post("/login",(req,res)=>{
+    const name = req.body.uname
+    res.send("user created:"+name);
+});
+app.listen(8088);
