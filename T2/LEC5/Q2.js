7470dd9a394b20220f7322a28969ef7ee336ca3f
Dhrumil <dhrumilvaghela22@gmail.com>
2026-04-22 09:42:08 +0530

    this is lec5 of T2

--- T2/LEC5/Q2.js
+++ T2/LEC5/Q2.js
@@ -1,2 +1,17 @@
 // wrote express script to get form data using get method and display data in json formte in next page /process.
 // create html and js file saparatly.
+
+
+const express = require("express");
+const app = express();
+
+app.use(express.static(__dirname));
+app.get("/process",(req,res)=>{
+    var fn = req.query.fname;
+    var sn = req.query.sname;
+    console.log(req.query)
+    res.send("welcome:"+fn+" "+sn);
+});
+app.listen(8008,()=>{
+    console.log("port running at 8008");
+})
